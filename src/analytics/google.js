760e9dfe2ef317cce78a54608117fbcb3fbb1b49
David Lozzi <lozzi.david@bcg.com>
2020-04-10 10:45:37 -0400

    added google

---
+++ src/analytics/google.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+
+
+const google = {
+  pageView: async () => {
+    // ga('set', 'page', window.location.href);
+    // ga('send', 'pageview');
+  }
+};
+
+export default google;
