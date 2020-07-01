72d4aa45c54b260e9139cfeefbb1c2f575d9cb8a
David Lozzi <lozzi.david@bcg.com>
2020-07-01 11:14:58 -0400

    clean up login form, made required

--- src/components/loginform/loginform.js
+++ src/components/loginform/loginform.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Button, InputGroup } from 'react-bootstrap';
+import { Button, InputGroup, Form } from 'react-bootstrap';
 import { v4 } from 'uuid';
 
 import analytics, { CATEGORIES } from 'analytics/analytics';
@@ -8,22 +8,29 @@ import FormControl from 'components/formcontrol/formcontrol';
 
 import { actions as MeActions } from 'redux/api/me/me';
 
-// TODO make both fields required
-
 const LoginForm = ({ roomName, onOpen }) => {
   const dispatch = useDispatch();
   const [myName, setMyName] = useState('');
   const [myUserId] = useState(v4());
+  const [validated, setValidated] = useState(false);
 
-  const openRoom = () => {
-    // #public is in index.html, very hacky approach but it works
-    document.getElementById('public').after(document.getElementById('root'));
-    document.getElementById('public').remove();
-    MeActions.set(myName, myUserId)(dispatch);
-    analytics.event('login', CATEGORIES.USER);
-    localStorage.setItem('myName', myName);
-    localStorage.setItem('myUserId', myUserId);
-    if (onOpen) onOpen();
+  const openRoom = (e) => {
+    const form = e.currentTarget;
+    if (form.checkValidity()) {
+      // #public is in index.html, very hacky approach but it works
+      document.getElementById('public').after(document.getElementById('root'));
+      document.getElementById('public').remove();
+      MeActions.set(myName, myUserId)(dispatch);
+      analytics.event('login', CATEGORIES.USER);
+      localStorage.setItem('myName', myName);
+      localStorage.setItem('myUserId', myUserId);
+      if (onOpen) onOpen();
+    } else {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
   };
 
   useEffect(() => {
@@ -34,18 +41,22 @@ const LoginForm = ({ roomName, onOpen }) => {
 
   return (
     <>
-      <h2 className="major">The room <span>{roomName}</span> is ready, ready to join?</h2>
-      <InputGroup className="mb-3">
-        <FormControl
-          placeholder="Your Name"
-          aria-label="Your Name"
-          value={myName}
-          onChange={e => setMyName(e.target.value)}
-        />
-        <InputGroup.Append>
-          <Button onClick={openRoom}>Let's Go!</Button>
-        </InputGroup.Append>
-      </InputGroup>
+      <h2 className="major">The room <span>{roomName}</span> is ready, do you want to join?</h2>
+      <div></div>
+      <Form noValidate validated={validated} onSubmit={openRoom}>
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="Your Name"
+            aria-label="Your Name"
+            value={myName}
+            onChange={e => setMyName(e.target.value)}
+            required
+          />
+          <InputGroup.Append>
+            <Button type="submit">Let's Go!</Button>
+          </InputGroup.Append>
+        </InputGroup>
+      </Form>
     </>
   )
 }
